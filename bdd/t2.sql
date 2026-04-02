7559957a23f5a94fa0bab693ed23dd94044410d7
ando-malala <rakotoniainaandomalala@gmail.com>
2026-04-02 14:45:17 +0300

    commit

--- bdd/t2.sql
+++ bdd/t2.sql
@@ -16,11 +16,11 @@ INSERT INTO hotel(nom) VALUES
 
 INSERT INTO distance(idLieuDepart, idLieuArrivee, distanceKm) VALUES
 (1,2,90),
-(1,3,35),
+(1,3,65),
 (2,1,90),
-(3,1,35),
-(2,3,60),
-(3,2,60);
+(3,1,65),
+(2,3,10),
+(3,2,10);
 
 
 insert into typeCarburant(libelle) values ('Diesel'),('Essence'),('Electrique'),('Hybride');
@@ -28,20 +28,17 @@ insert into typeCarburant(libelle) values ('Diesel'),('Essence'),('Electrique'),
 insert into unite(libelle) values ('km/h'),('minutes');
 
 insert into parametre(libelle,valeur,idUnite) VALUES
-('Vitesse moyenne',50,1),
+('Vitesse moyenne',60,1),
 ('Temps d attente' , 30,2);
 
 INSERT INTO vehicule(reference,capacite,heure_dispo,typeCarburantId) VALUES
-('vehicule1', 5, '09:00', 1),
-('vehicule2', 5,  '09:00', 2),
-('vehicule3', 12,  '00:00', 1),
-('vehicule4', 9, '09:00', 1),
-('vehicule5', 12, '13:00', 2);
+('vehicule1', 10, '00:00', 1),
+('vehicule2', 8,  '08:00', 1),
+('vehicule3', 8,  '08:00', 2),
+('vehicule4', 12, '09:00', 2);
 
 INSERT INTO reservation(idHotel, idClient, nbPassager, dateHeure) VALUES
-(2, 'client1', 7, '2026-03-19 09:00'),
-(3, 'client2', 20,  '2026-03-19 08:00'),
-(2, 'client3', 3,  '2026-03-19 09:10'),
-(2, 'client4', 10,  '2026-03-19 09:15'),
-(2, 'client5', 5,  '2026-03-19 09:20'),
-(2, 'client6', 12,  '2026-03-19 13:30');
+(2, 'client1', 20, '2026-04-02 06:00'),
+(2, 'client2', 6,  '2026-04-02 08:15'),
+(2, 'client3', 10,  '2026-04-02 09:00'),
+(3, 'client4', 6,  '2026-04-02 09:10');
